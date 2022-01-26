85ddf4a41c26c9f88266eb3e874a6b824e321da6
Camila Kunitz <camila.kunitz@gmail.com>
2022-01-25 22:25:31 -0300

    Cria card de notícia

--- src/pages/Noticias/Noticias.js
+++ src/pages/Noticias/Noticias.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Pesquisa from '../../components/Pesquisa/Pesquisa'
+import Card from './Card/Card';
 import { Titulo, PesquisaContainer } from './Noticias.styles'
 
 const Noticias = () => {
@@ -9,6 +10,9 @@ const Noticias = () => {
       <PesquisaContainer>
         <Pesquisa name="noticia" label="Pesquise uma notícia" type="text" placeholder="Digite o título de uma notícia.." />
       </PesquisaContainer>
+      <Card />
+      <Card />
+      <Card />
     </>
   )
 };
